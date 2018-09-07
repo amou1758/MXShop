f2fe74a13a3d5005fd7297546703822ad18d5e2a
amou1758 <amou1758@163.com>
2018-09-08 04:07:24 +0800

    13. Django 的 serializer 序列化 model!

--- README.md
+++ README.md
@@ -1014,3 +1014,30 @@ class GoodsListView(View):
 
 ```
 
+
+
+### 2. Django 的 serializer 序列化 model
+
+使用 Django 的 serializer 
+
+```python
+from django.views.generic.base import View
+
+from goods.models import Goods
+
+
+class GoodsListView(View):
+    def get(self, request):
+        """
+        通过 django 的 view 实现商品列表页
+        """
+        goods = Goods.objects.all()[:10]
+        import json
+        from django.core import serializers
+        # django 为我们提供的方法, 专门做序列化的
+        json_data = serializers.serialize('json', goods)
+        json_data = json.loads(json_data)
+        from django.http import JsonResponse
+        return JsonResponse(json_data, safe=False)
+```
+
--- apps/goods/views_base.py
+++ apps/goods/views_base.py
@@ -9,18 +9,13 @@ class GoodsListView(View):
     def get(self, request):
         """
         通过 django 的 view 实现商品列表页
-        :param request:
-        :return:json.dumps(xxx)
         """
-        json_list = []
         goods = Goods.objects.all()[:10]
-        for good in goods:
-            json_dict = {}
-            json_dict['name'] = good.name
-            json_dict['category'] = good.category.name
-            json_dict['market_price'] = good.market_price
-            json_list.append(json_dict)
-        print(json_list)
-        from django.http import HttpResponse
         import json
-        return HttpResponse(json.dumps(json_list), content_type='application/json')
+        from django.core import serializers
+        # django 为我们提供的方法, 专门做序列化的
+        json_data = serializers.serialize('json', goods)
+        json_data = json.loads(json_data)
+        from django.http import JsonResponse
+        return JsonResponse(json_data, safe=False)
+
